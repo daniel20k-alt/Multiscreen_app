13eee162eea6d79463e547ff0c92d605956c10ee
Testing_author <testing_beta@mail.com>
2020-09-27 23:25:15 +0300

    Initial Commit

---
+++ Multiscreen_appTests/Multiscreen_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  Multiscreen_appTests.swift
+//  Multiscreen_appTests
+//
+//  Created by DDDD on 27/09/2020.
+//
+
+import XCTest
+@testable import Multiscreen_app
+
+class Multiscreen_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
